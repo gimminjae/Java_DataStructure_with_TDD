cc8fc668f9970a0079b4d4e568846cbdf525c1f7
gimminjae <min356812@naver.com>
2022-11-14 17:29:22 +0900

    Feat: get, add 메서드 구현 위해 MyArrayList에 size, lastIndex변수 추가

--- src/main/java/com/example/java_data_structure/list/MyArrayList.java
+++ src/main/java/com/example/java_data_structure/list/MyArrayList.java
@@ -1,10 +1,21 @@
 package com.example.java_data_structure.list;
 
 public class MyArrayList {
-    public void test() {
-        System.out.println("test");
-    }
+    private int[] array;
+    private long size;
+    private long lastIndex;
+
     public MyArrayList() {
+        array = new int[10];
+        size = 0;
+        lastIndex = 0;
+    }
+
+    public int get(int n) {
+        return array[n];
+    }
+
+    public void add(int n) {
 
     }
 }
